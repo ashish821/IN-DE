cba9e0c279144b7000b18ed34ed863189b0defae
Ashish Srivastav <ashishshrivastav275@gmail.com>
2022-03-10 15:36:38 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
-      <h3>Welcome To Coding Life</h3>
+    <div class="container">
+      <h3> Welcome To Coding World </h3>
+      <h5>BY clicking you Can get the Value</h5>
+
+      <div class="quantity">
+        <a class="quantity_minus" title="Decrement">
+          <span>-</span>
+        </a>
+        <input name="quantity" type="text" class="quantity_input" value="0" />
+        <a class="quantity_plus" title="Increment">
+          <span>+</span>
+        </a>
+      </div>
     </div>
   );
 }
